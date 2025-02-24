0d0427ce1e1afb1b17c09a273d7aadfd8075e544
loci887774 <lcuc887774@gmail.com>
2025-02-24 21:09:33 +0700

    chinh sua buoi 1

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hello</title>
+
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
 </head>
 <body>
     <h1>hello world</h1>
